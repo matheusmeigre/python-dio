69e636297f01cde36add69ae7e9a4d2501c4dd1f
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-12 11:23:00 -0300

    assignment operators class with comments

---
+++ opAtribuicao.py
@@ -0,0 +1,32 @@
+#Sinal de atribuição ( = )
+saldo = 500
+print(saldo)
+
+#Sinal de atribuição por adição ( += )
+saldo = 500
+saldo += 300
+print(saldo)
+
+#Sinal de atribuição por subtração ( -= )
+saldo = 500
+saldo -= 300
+print(saldo)
+
+#Sinal de atribuição por divisão ( /= )
+saldo = 500
+saldo /= 5
+print(saldo)
+
+saldo = 500
+saldo //= 10
+print(saldo)
+#Sinal de atribuição por módulo ( %= )
+saldo = 500
+saldo %= 300
+print(saldo)
+
+#Sinal de atribuição por exponenciação ( **= )
+saldo = 500
+saldo **= 4
+print(saldo)
+
