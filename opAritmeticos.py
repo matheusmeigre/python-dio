f4198f7212c6d2654ef8cffe3f4fe4e8022574de
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-12 11:08:16 -0300

    arithmetic operators class

---
+++ opAritmeticos.py
@@ -0,0 +1,24 @@
+produto_1 = 10
+produto_2 = 20
+
+soma = (produto_1 + produto_2)
+print(soma)
+
+subtracao = (produto_1 - produto_2)
+print(subtracao)
+
+divisao = (produto_1 / produto_2)
+print(divisao)
+
+multiplicacao = (produto_1 * produto_2)
+print(multiplicacao)
+
+modulo = (produto_1 % produto_2)
+print(modulo)
+
+divisao_inteira = (produto_1 // produto_2)
+print(divisao_inteira)
+
+exponenciacao = (produto_1 ** produto_2)
+print(exponenciacao)
+
