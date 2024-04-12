2eea45ddeb464a91d1ebf7f5285f114adb9f5ccf
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-12 11:15:33 -0300

    compare operators with comments to identify

---
+++ opComparacao.py
@@ -0,0 +1,9 @@
+saldo = 400
+saque = 200
+
+print(saldo == saque) #Saldo é igual a saque?
+print(saldo != saque) #Saldo é diferente ou igual a saque?
+print(saldo > saque)  #Saldo é maior do que saque?
+print(saldo >= saque) #Saldo é maior ou igual a saque?
+print(saldo < saque)  #Saldo é menor do que saque?
+print(saldo <= saque) #Saldo é menor ou igual a saque?
