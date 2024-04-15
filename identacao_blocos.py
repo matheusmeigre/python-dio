9e3a80606a2814671350be5f12707461a02c12c0
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-15 14:53:27 -0300

    indentation class

---
+++ identacao_blocos.py
@@ -0,0 +1,17 @@
+def sacar(valor):  # Abertura de bloco do método sacar
+    saldo = 500
+
+    if saldo >= valor: # Abertura de bloco do if
+        print("valor sacado!")
+        print("retire o valor no caixa.")
+
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")
+
+
+def depositar(valor):   # Abertura de bloco do método depositar
+    saldo = 500
+    saldo += valor
+                            # Fechamento de bloco
+
+
+sacar(600)
