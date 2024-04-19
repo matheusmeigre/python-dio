73612dc2a499ac06ae74791d2444e6fd6e7a6011
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-18 21:09:46 -0300

    slice in lists

--- listas/listas.py
+++ listas/listas.py
@@ -1,1 +1,15 @@
-lista = ["p","y"]
+"""
+Fatiamento de strings
+ É uma tecnica utilizada para retornar substrings(partes da string original), informando
+ INICIO(START), FIM(STOP) e [PASSO(STEP)]
+ Exemplo:
+ lista[inicio:fim:step]
+"""
+lista = ["p","y","t","h","o","n"]
+
+lista[2:] # ["t", "h", "o", "n"]
+lista[:2] # ["p", "y"]
+lista[1:3] # ["y", "t"]
+lista[0:3:2] # ["p", "t"]
+lista[::] # ["p", "y", "t", "h", "o", "n"]
+lista[::-1] # ["n", "o", "h", "t", "y", "p"]
