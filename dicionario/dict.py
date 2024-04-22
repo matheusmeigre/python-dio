cc03d43621d99a02829918650218ed74d39a6a75
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-22 17:20:29 -0300

    create and access datas

--- dicionario/dict.py
+++ dicionario/dict.py
@@ -19,8 +19,27 @@ dados["nome"] # "Matheus"
 dados["idade"] # 22
 dados["celular"] # "3333-4444"
 
+# Sobreescrevendo o valor do dicionário dados
+
 dados["nome"] = "Lais"
 dados["idade"] = 20
 dados["celular"] = "2222-3333"
 
 dados # {"nome": "Lais", "idade": 20, "celular": "2222-3333"}
+
+"""
+Dicionários aninhados
+ Eles podem armazenar qualquer tipo de objeto Python como valor, desde que a chave para
+ esse valor seja um objeto imutável como (strings e números).
+"""
+
+contato = {
+    "matheus@gmail.com": {"nome": "Matheus", "celular": "3333-4444"},
+    "marcos@gmail.com": {"nome": "Marcos", "celular": "4444-5555", "profissao": {"atual": "Professor"}},
+    "julio@gmail.com": {"nome": "Julio", "celular": "5555-6666"},
+}
+
+celular = contato["matheus@gmail.com"]["celular"] # "3333-4444"
+
+profissao_atual = contato["marcos@gmail.com"]["profissao"]["atual"]
+print(profissao_atual)
