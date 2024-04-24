8cdac745ee668c97e4468e67d6a104b78989941f
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-24 18:48:27 -0300

    function class added

---
+++ 01 - Estrutura de dados/funcao/primeiras_funcoes.py
@@ -0,0 +1,12 @@
+def exibir_mensagem():
+    print("Olá, mundo!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}") # Obrigatório passar o valor do parâmetro no ato da chamada
+
+
+def exibir_mensagem_3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}")
+
+exibir_mensagem_3()
+exibir_mensagem_3(nome="Matheus") # or exibir_mensagem_3("Matheus")
