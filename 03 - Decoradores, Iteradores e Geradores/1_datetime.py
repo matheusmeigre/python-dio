f069b2a52b00b1de7e53814bce2d5683b2d57d87
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-05-23 13:19:31 -0300

    datetime class

---
+++ 03 - Decoradores, Iteradores e Geradores/1_datetime.py
@@ -0,0 +1,12 @@
+from datetime import date, datetime, time
+
+data = date.today()
+print(data)
+
+data_hora = datetime(2024, 5, 23)
+print(data_hora)
+
+print(datetime.today())
+
+hora = time(13, 18, 0)
+print(hora)
